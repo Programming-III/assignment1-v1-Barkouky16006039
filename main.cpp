1c88d97e39b410d1e251087973a1e73f74ada865
Barkouky16006039 <eyad.barkouky@gmail.com>
2025-11-12 14:55:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,87 @@
 #include <iostream>
 using namespace std;
 
+class animal{
+    
+    private:
+    
+    string name;
+    int age;
+    bool isHungry;
+    
+    public:
+    
+    void display(){
+        cout << name << endl;
+        cout << age << endl;
+        cout << isHungry << endl;
+    }
+    void feed();
+    
+}
+
+class mammal :: animal{
+    
+    public:
+    
+    string furColor;
+    
+}
+
+class bird :: animal{
+    
+    public:
+    
+    float wingSpan
+}
+
+class reptile :: animal{
+    
+    public:
+    
+    bool isVenomous;
+    
+}
+
+class enclosure{
+    
+    private:
+    
+    int capacity;
+    int currentCount
+    
+    public:
+    
+    void addAnimal(animal* a){
+        
+        
+        
+    }
+    void displayAnimals(){
+        
+        
+        
+    }
+    
+}
+
+class visitor{
+    
+    private:
+    
+    string visitorName;
+    int ticketsBought;
+    
+    public:
+    
+    void displayInfo(){
+        
+        cout << visitorName << endl;
+        cout << ticketsBought << endl;
+        
+    }
+    
+}
 
 // ============== MAIN FUNCTION ==============
 int main() {
